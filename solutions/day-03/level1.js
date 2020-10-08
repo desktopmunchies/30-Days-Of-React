bb1ae677d1f959f532737554f67f062c7d0fa278
abc <abc@example.com>
2020-10-08 16:30:32 +0300

    chore: exercise level1 complete

---
+++ solutions/day-03/level1.js
@@ -0,0 +1,14 @@
+// 1. a module is function which can exported/imported and used when needed
+// 2. a package is a module or a collectiono modules
+// 3. one can contain more of the other
+// 4. node package manager - a package manager
+// 5. a module bundler
+// 6. npx create-nreact-app
+// 7. package.json - list of packages
+//    package-lock.json - locked version of packages
+//    .gitignore - files to be excluded from github
+//    node_modules - files needed from node
+//    public - files that appear in the browser
+// 8. visual studio code
+// 9. done
+// 10. done
